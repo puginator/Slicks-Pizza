c0315308d6ad085152870ccc256447246fdbf38d
Austin Amento <puggsy31@aol.com>
2021-12-17 14:40:46 -0800

    added typography to global styles

--- src/pages/slicemasters.js
+++ src/pages/slicemasters.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function SliceMastersPage() {
   return (
     <>
-      <Nav />
       <p>Hey I'm the Slice Masters Page page</p>
     </>
   );
