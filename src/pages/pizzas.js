6970bfeb6cab92da0f84ca426c7e7e50d8a12ba0
Austin Amento <puggsy31@aol.com>
2021-12-17 11:48:49 -0800

    added layout

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 export default function PizzasPage() {
   return (
-    <div>
+    <Layout>
       <p>Hey I'm the Pizza page</p>
-    </div>
+    </Layout>
   );
 }
